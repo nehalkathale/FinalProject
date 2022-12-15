7d42349b70ac7128a989d9c4a51783736ee7441e
Nehalk145 <nkathale@uncc.edu>
2022-12-14 22:49:50 -0500

    Removed text

--- googleapi.js
+++ googleapi.js
@@ -36,10 +36,5 @@ const detectLandmark = async (file_path) => {
 
 };
 
-const detectText = async (file_path) => {
 
-    let [result] = await client.textDetection(file_path);
-    console.log(result.fullTextAnnotation.text);
-};
-
-module.exports = { detectLandmark, detectText }
+module.exports = { detectLandmark }
